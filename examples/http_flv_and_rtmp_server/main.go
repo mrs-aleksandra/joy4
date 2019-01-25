baea4df4b4f401a6f83d70f96f59669db0d0b635
paulo@homeland <paulo.martins@aukbit.com>
2019-01-25 00:47:30 +0000

    test rtmp server

--- examples/http_flv_and_rtmp_server/main.go
+++ examples/http_flv_and_rtmp_server/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
-	"sync"
+	"fmt"
 	"io"
 	"net/http"
-	"github.com/nareix/joy4/format"
+	"sync"
+
 	"github.com/nareix/joy4/av/avutil"
 	"github.com/nareix/joy4/av/pubsub"
-	"github.com/nareix/joy4/format/rtmp"
+	"github.com/nareix/joy4/format"
 	"github.com/nareix/joy4/format/flv"
+	"github.com/nareix/joy4/format/rtmp"
 )
 
 func init() {
@@ -39,10 +41,14 @@ func main() {
 		ch := channels[conn.URL.Path]
 		l.RUnlock()
 
+		fmt.Println("HandlePlay", conn.URL.Path, ch, channels)
+
 		if ch != nil {
 			cursor := ch.que.Latest()
 			avutil.CopyFile(conn, cursor)
 		}
+
+		fmt.Println("HandlePlay finish", conn.URL.Path, ch, channels)
 	}
 
 	server.HandlePublish = func(conn *rtmp.Conn) {
@@ -63,22 +69,31 @@ func main() {
 			return
 		}
 
+		fmt.Println("HandlePublish", conn.URL.Path, ch, channels)
+
 		avutil.CopyPackets(ch.que, conn)
+
+		fmt.Println("CopyPackets", conn.URL.Path, ch, channels)
 
 		l.Lock()
 		delete(channels, conn.URL.Path)
 		l.Unlock()
 		ch.que.Close()
+
+		fmt.Println("Close", conn.URL.Path, ch, channels)
 	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
 		l.RLock()
 		ch := channels[r.URL.Path]
 		l.RUnlock()
 
+		fmt.Println(r.URL.Path, ch)
+
 		if ch != nil {
 			w.Header().Set("Content-Type", "video/x-flv")
-			w.Header().Set("Transfer-Encoding", "chunked")		
+			w.Header().Set("Transfer-Encoding", "chunked")
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.WriteHeader(200)
 			flusher := w.(http.Flusher)
@@ -87,18 +102,27 @@ func main() {
 			muxer := flv.NewMuxerWriteFlusher(writeFlusher{httpflusher: flusher, Writer: w})
 			cursor := ch.que.Latest()
 
+			fmt.Println("HandleFunc", cursor)
+
 			avutil.CopyFile(muxer, cursor)
 		} else {
 			http.NotFound(w, r)
 		}
 	})
 
-	go http.ListenAndServe(":8089", nil)
+	go http.ListenAndServe(":8080", nil)
 
 	server.ListenAndServe()
 
-	// ffmpeg -re -i movie.flv -c copy -f flv rtmp://localhost/movie
+	// ffmpeg -re -i manuel.mp4 -c copy -f flv rtmp://localhost/movie
 	// ffmpeg -f avfoundation -i "0:0" .... -f flv rtmp://localhost/screen
-	// ffplay http://localhost:8089/movie
-	// ffplay http://localhost:8089/screen
+	// ffmpeg -f avfoundation -i "0:0" -f flv rtmp://localhost/screen
+
+	// default camera low quality
+	// NOTE: "0:0" means default camera and audio, specify size, video and audio codec
+	// ffmpeg -f avfoundation -i "0:0" -s 768x432 -vcodec h264 -acodec mp2 out.mp4
+	// ffmpeg -f avfoundation -i "0:0" -s 768x432 -f flv rtmp://localhost/screen
+
+	// ffplay http://localhost:8088/movie
+	// ffplay http://localhost:8088/screen
 }
